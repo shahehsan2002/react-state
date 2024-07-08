a8d78595dd14047f2daef5ec132f9904a3300841
Shah Ehsan <shahehsan2002@gmail.com>
2024-07-08 14:18:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CounterWithClass from "./components/CounterWithClassComponents"
+
+function App() {
+  return (
+    <div>
+      <CounterWithClass/>
+    </div>
+  )
+}
+
+export default App
